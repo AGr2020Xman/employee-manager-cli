e5325eaf7e56aa5325000c6192edbf2c07fe8b5f
Andre Grech <silver.grech@gmail.com>
2020-11-09 21:44:07 +1100

    view queries and functions settled. to unpack in a switch case model for simple access and less function calls

--- viewElement.js
+++ viewElement.js
@@ -1,5 +1,6 @@
-
+const { prompt } = require('inquirer');
 const databaseQuery = require('./index');
+const conTable = require('console.table')
 
 const viewDepartment = () => {
     const dept_query = `
@@ -27,6 +28,7 @@ const viewEmployees = () => {
         SELECT 
             CONCAT(employees.first_name, " ", employees.last_name) Name, 
             employees.id ID,
+            CONCAT(employees.first_name, " ", employees.last_name) Name, 
             role.title Role, 
             department.name Department,
             role.salary Salary,
@@ -40,12 +42,71 @@ const viewEmployees = () => {
     console.table(resultsArray);
 };
 
-const viewEmployeesByManager = () => {
+const managerID = async () => {
 
+	const manager_query = `
+        SELECT 
+            id Value, 
+            CONCAT(first_name, " ", last_name) Name
+		FROM employee
+		WHERE ISNULL(manager_id)
+		ORDER BY Name`;
+
+	const managersResult = await databaseQuery(manager_query);
+
+	// Configure inquirer parameters.
+	const managerChoice = {
+		type: "list",
+		name: "id",
+		message: "Choose a manager:\n",
+		pageSize: 30,
+		choices: managersResult,
+	};
+
+	// Return the employee id of the manager.
+	const answers = await prompt(managerChoice);
+	return answers.id;
+}
+
+
+const viewEmployeesByManager = () => {
+    	console.clear();
+		writeHeader("Employees by Manager");
+		const managerID = await managerID();
+		empman_query = `
+            SELECT 
+                CONCAT(employees.first_name, " ", employees.last_name) Name, 
+                employees.id ID, 
+                CONCAT(employees.first_name, " ", employees.last_name) Name, 
+                role.title Role, 
+                department.name Department, 
+                CONCAT(m.first_name, " ", m.last_name) Manager, 
+                role.salary Salary
+			FROM employees
+			LEFT JOIN employees m ON m.id = employees.manager_id
+			JOIN role ON role.id = employees.role_id
+			JOIN department ON department.id = role.department_id
+			WHERE employees.manager_id = ${managerID}
+            ORDER BY Name`;
+            
+        const resultsArray = databaseQuery(empman_query);
+        console.table(resultsArray);
 };
 
 const viewBudget = () => {
-
+    budget_query = `
+        SELECT 
+            department.name Department, 
+            department.id ID, 
+            SUM(role.salary) 'Utilised Budget'
+		FROM employees
+		JOIN role ON employees.role_id = role.id
+		JOIN department ON role.department_id = department.id
+		GROUP BY department.id
+        ORDER BY Department`;
+        
+    const resultsArray = databaseQuery(budget_query);
+    console.table(resultsArray);
 };
 
 module.exports = { viewEmployees, viewRoles, viewDepartment, viewEmployeesByManager, viewBudget };
