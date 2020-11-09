c008da05b22ca18e8aa43f771e9201628c9e8567
Andre Grech <silver.grech@gmail.com>
2020-11-09 16:23:18 +1100

    storage arrays for operations

---
+++ db_array.js
@@ -0,0 +1,26 @@
+// existing departments
+const departmentArray = [];
+
+// department.name from arrays.deptArray
+const filterDepartmentArray = [];
+
+// all roles
+const roleArray = [];
+
+// role.title from arrays.roleArray
+const filterRoleArray = [];
+
+// all employees.
+const employeeArray = [];
+
+// all managers
+const managerArray = [];
+
+module.exports = { 
+    departmentArray, 
+    filterDepartmentArray, 
+    roleArray, 
+    filterRoleArray, 
+    employeeArray, 
+    managerArray 
+};
