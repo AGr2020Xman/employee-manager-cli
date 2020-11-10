07ef101346c0624c8e48d50c70db8dfb845d6cad
Andre Grech <silver.grech@gmail.com>
2020-11-10 14:05:42 +1100

    index file to run application

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
-const mysql = require('mysql');
-const { prompt } = require('inquirer');
+const { displayLogoArt } = require('./headers');
+const { startMenu } = require('./startCLI');
 
-const connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "H0n@s0up1234",
-    database: "employee_tracker_db"
-});
+const init = () => {
+    displayLogoArt();
+    startMenu();
+};
 
-connection.connect((err) => {
-    if (err) throw err;
-})
+init();
