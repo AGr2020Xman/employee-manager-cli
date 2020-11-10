05ef317dd6399cd765f8ec7f74e9ff64778c9af7
Andre Grech <silver.grech@gmail.com>
2020-11-10 14:06:05 +1100

    future inclusion to pull out repetitive list files from primary add/delete/update fx's

---
+++ listFunctions.js
@@ -0,0 +1,50 @@
+const databaseQuery = require('./databaseQuery');
+
+const listEmployees = async () => {
+
+	const query = `
+		SELECT id AS value, CONCAT(first_name, " ", last_name) AS name
+		FROM employees
+		ORDER BY name`;
+
+	const employeesArray = await databaseQuery(query);
+	return employeesArray;
+};
+
+const listRoles = async () => {
+
+	const query = `
+		SELECT id AS value, title AS name
+		FROM role
+		ORDER BY name`;
+
+	const rolesArray = await databaseQuery(query);
+	return rolesArray;
+};
+
+const listManagers = async () => {
+
+	const manager_query = `
+		SELECT id AS value, CONCAT(first_name, " ", last_name) AS name
+		FROM employee
+		WHERE ISNULL(manager_id)
+		ORDER BY ID
+	`;
+
+	const resultsArray = await databaseQuery(manager_query);
+	return resultsArray;
+};
+
+
+const listDepartments = async () => {
+
+	const dept_query = `
+    SELECT id AS value, name
+    FROM department
+    ORDER BY ID`;
+
+	const resultsArray = await databaseQuery(dept_query);
+	return resultsArray;
+};
+
+module.exports = { listEmployees, listRoles, listManagers, listDepartments };
