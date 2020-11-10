e5249d7b6fcc28d3a0c5eb9d1c8742c230e725b3
Andre Grech <silver.grech@gmail.com>
2020-11-10 14:24:38 +1100

    reformatting to allow access of role table BEFORE employees is created

--- schema.sql
+++ schema.sql
@@ -1,27 +1,15 @@
+DROP DATABASE IF EXISTS employee_tracker_db;
 CREATE DATABASE employee_tracker_db;
 
 USE employee_tracker_db;
 
-CREATE TABLE employees (
-    id INT NOT NULL AUTO_INCREMENT,
-    first_name VARCHAR(30),
-    last_name VARCHAR(30),
-    role_id INT NULL, 
-    manager_id INT NULL,
-    CONSTRAINT fk_role
-        FOREIGN KEY(role_id) 
-        REFERENCES role(id)
-        ON UPDATE CASCADE
-        ON DELETE SET NULL,
-    CONSTRAINT fk_manager
-        FOREIGN KEY(manager_id) 
-        REFERENCES employees(id)
-        ON UPDATE CASCADE
-        ON DELETE SET NULL,
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT, 
+    name VARCHAR(30) NOT NULL,
     PRIMARY KEY(id)
 );
 
-CREATE TABLE role (
+CREATE TABLE `role` (
     id INT NOT NULL AUTO_INCREMENT,
     title VARCHAR(30),
     salary DECIMAL(10,2) NOT NULL,
@@ -34,8 +22,21 @@ CREATE TABLE role (
     PRIMARY KEY(id),
 );
 
-CREATE TABLE department (
-    id INT NOT NULL AUTO_INCREMENT, 
-    name VARCHAR(30) NOT NULL,
+CREATE TABLE employees (
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT NULL, 
+    manager_id INT NULL,
+    CONSTRAINT fk_role
+        FOREIGN KEY(role_id) 
+        REFERENCES `role`(id)
+        ON UPDATE CASCADE
+        ON DELETE SET NULL,
+    CONSTRAINT fk_manager
+        FOREIGN KEY(manager_id) 
+        REFERENCES employees(id)
+        ON UPDATE CASCADE
+        ON DELETE SET NULL,
     PRIMARY KEY(id)
 );
