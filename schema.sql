94d604278f5563ff08b3fa9e168a1f3fce10a8ca
Andre Grech <silver.grech@gmail.com>
2020-11-10 14:21:04 +1100

    changes made to schema. formatting errors. + role_id FROM employees must be NULL, as it is a FK

--- schema.sql
+++ schema.sql
@@ -4,10 +4,10 @@ USE employee_tracker_db;
 
 CREATE TABLE employees (
     id INT NOT NULL AUTO_INCREMENT,
-    first_name VARCHAR(30);
-    last_name VARCHAR(30);
-    role_id INT NOT NULL, 
-    manager_id INT,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT NULL, 
+    manager_id INT NULL,
     CONSTRAINT fk_role
         FOREIGN KEY(role_id) 
         REFERENCES role(id)
@@ -18,11 +18,11 @@ CREATE TABLE employees (
         REFERENCES employees(id)
         ON UPDATE CASCADE
         ON DELETE SET NULL,
-    PRIMARY KEY(id),
+    PRIMARY KEY(id)
 );
 
 CREATE TABLE role (
-    id AUTO_INCREMENT,
+    id INT NOT NULL AUTO_INCREMENT,
     title VARCHAR(30),
     salary DECIMAL(10,2) NOT NULL,
     department_id INT NOT NULL,
@@ -35,7 +35,7 @@ CREATE TABLE role (
 );
 
 CREATE TABLE department (
-    id INT AUTO_INCREMENT, 
+    id INT NOT NULL AUTO_INCREMENT, 
     name VARCHAR(30) NOT NULL,
     PRIMARY KEY(id)
 );
