6f5900d405cce47e9337afc5458cab389c056d37
Andre Grech <silver.grech@gmail.com>
2020-11-10 16:09:12 +1100

    seed needed to be adjusted to available data at seeding

--- seed.sql
+++ seed.sql
@@ -3,15 +3,16 @@ VALUES ("Senate"), ("Council"), ("Diplomacy"), ("Knights");
 
 INSERT INTO role (title, salary, department_id)
 VALUES ("Senator", 55000, 1),
-("Jedi Master", 65000, 1),
-("Jedi Knight", 60000, 2);
+("Jedi Master", 65000, 2),
+("Jedi Knight", 60000, 3),
+("Jedi Sentinel", 60000, 4);
 
 INSERT INTO employees (first_name, last_name, role_id, manager_id)
-VALUES ("Qui Gon", "Jinn", 9, null),
-("Mace", "Windu", 6, null),
-("Obi Wan", "Kenobi", 2, null),
-("Anakin", "Skywalker", 3, null),
-("Aayla", "Secura", 4, 4),
-("Luke", "Skywalker", 5, 4),
-("Leia", "Organa", 1, 3),
-("Han", "Solo", 7, 2);
+VALUES ("Qui Gon", "Jinn", 1, null),
+("Mace", "Windu", 2, null),
+("Obi Wan", "Kenobi", 3, null),
+("Anakin", "Skywalker", 4, null),
+("Aayla", "Secura", 1, 4),
+("Luke", "Skywalker", 2, 3),
+("Leia", "Organa", 3, 2),
+("Han", "Solo", 4, 1);
