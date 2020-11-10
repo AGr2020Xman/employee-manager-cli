9fc2a6f5edb3840fb340e4bb3571fd553a202bda
Andre Grech <silver.grech@gmail.com>
2020-11-11 00:26:51 +1100

    fixed update

--- src/lib/updateEmployee.js
+++ src/lib/updateEmployee.js
@@ -54,27 +54,26 @@ const updateDetails = async () => {
 		choices: await listRoles(),
 		pageSize: 12
 		},
-		{
-		type: "list",
-		name: "department_id",
-		message: "Choose the employee's new department:\n",
-		when: (answers) => answers.field === "Department",
-		choices: await listDepartments(),
-		pageSize: 12,
-		},
+		// {
+		// type: "list",
+		// name: "department_id",
+		// message: "Choose the employee's department:\n",
+		// when: (answers) => answers.field === "Department",
+		// choices: await listDepartments(),
+		// pageSize: 12,
+		// },
 		{
 		type: "confirm",
 		name: "manager_yn",
 		message: "Do you wish to make this employee a manager? \n",
 		when: (answers) => answers.field === "Manager",
-		default: 'true',
 		pageSize: 10
 		},
 		{
 		type: "list",
 		name: "manager_id",
 		message: "Choose the employee's manager:\n",
-		when: (answers) => answers.manager_yn === "false",
+		when: (answers) => answers.manager_yn === false,
 		choices: await listManagers(),
 		pageSize: 10
 		}
@@ -86,20 +85,34 @@ const updateDetails = async () => {
 
 const updateEmployee = async () => {
 	console.clear();
-	
 	let updatedDetails = await updateDetails();
-	delete updatedDetails['manager_yn'];
-	updatedDetails['manager_id'] = this.id;
-    let updatedValuesArray = [updatedDetails.id];
+	console.log(updatedDetails);
+	let update_query = `UPDATE employees SET ? WHERE id=?`;
+	let updatedValuesArray = [updatedDetails.id];
+	if (('manager_yn' in updatedDetails) === true){
+		update_query = `UPDATE employees SET manager_id=? WHERE id=?`;
+		delete updatedDetails['manager_yn'];
+		updatedValuesArray.unshift(updatedDetails.manager_id)
+	} else if (('manager_yn' in updatedDetails) === false) {
+		update_query = `UPDATE employees SET ? WHERE id=?`;
+		delete updatedDetails['manager_yn'];
+		updatedDetails['manager_id'] = this.id;
+		updatedValuesArray.unshift(updatedDetails);
+	// } else if ('department_id' in updatedDetails) {
+	// 	update_query = `UPDATE employees SET department_id=? WHERE id=?`;
+	// 	updatedValuesArray.unshift(updatedDetails.department_id)
+	// 	console.log('inside dept_id',updatedValuesArray);		
+	} 
+	else if ('role_id' in updatedDetails) {
+		update_query = `UPDATE employees SET role_id=? WHERE id=?`
+		updatedValuesArray.unshift(updatedDetails.role_id)
+		console.log('inside roleid',updatedValuesArray);
+	}
 
 	delete updatedDetails.id;
-	delete updatedDetails.field;
-
-	updatedValuesArray.unshift(updatedDetails);	
-	const update_query = `UPDATE employees SET ? WHERE id=?`;
+	delete updatedDetails.field;	
 	await databaseQuery(update_query, updatedValuesArray);
 	
-	console.clear();
 	console.log('\n Task complete!');
 	console.log(` Employee updated.` + `\n`);
 };
